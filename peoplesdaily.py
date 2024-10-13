4cb28a8db432bfab43881dd529d4b9e3a202db98
Huaimin <112915548+TeAmo-Lemon@users.noreply.github.com>
2024-10-13 16:38:28 +0800

    Update peoplesdaily.py

--- peoplesdaily.py
+++ peoplesdaily.py
@@ -1,184 +1,142 @@
-import requests
-import bs4
-import os
-import datetime
+import aiohttp
+import asyncio
 import time
+from pyquery import PyQuery as pq
+from urllib.parse import urljoin
+import os
+import re
+import aiofiles  # 用于异步文件操作
+from datetime import datetime, timedelta
+
+CONCURRENCY = 4
+BASE_URL = 'http://paper.people.com.cn/rmrb/html/'
+semaphore = asyncio.Semaphore(CONCURRENCY)
 
 
-def fetchUrl(url, retries=3):
-    '''
-    功能：访问 url 的网页，获取网页内容并返回，失败时重试
-    参数：目标网页的 url，重试次数
-    返回：目标网页的 html 内容
-    '''
-    headers = {
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',
-    }
-    for i in range(retries):
-        try:
-            r = requests.get(url, headers=headers, timeout=10)
-            r.raise_for_status()
-            r.encoding = r.apparent_encoding
-            return r.text
-        except requests.RequestException as e:
-            print(f"Error fetching {url}: {e}")
-            if i < retries - 1:
-                print("Retrying...")
-                time.sleep(10)  # 请求失败，延时10秒再重试
-            else:
-                print(f"Failed after {retries} attempts.")
-                return None
+async def scrape_api(url):
+    async with semaphore:
+        print('scraping', url)
+        retries = 2
+        for attempt in range(retries):
+            try:
+                async with session.get(url) as response:
+                    if response.status == 200:
+                        return await response.text()
+                    else:
+                        print(f"Failed to fetch {url}, status code: {response.status}")
+            except Exception as e:
+                print(f"Error fetching {url}: {e}")
+            if attempt < retries - 1:
+                print(f"Retrying {url} in 5 seconds...")
+                await asyncio.sleep(5)  # 等待20秒再重试
+        log_failure(url)  # 记录失败的请求
+        return None
 
 
-def getPageList(year, month, day):
-    '''
-    功能：获取当天报纸的各版面的链接列表
-    参数：年，月，日
-    '''
-    url = f'http://paper.people.com.cn/rmrb/html/{year}-{month}/{day}/nbs.D110000renmrb_01.htm'
-    html = fetchUrl(url)
-    if not html:
+def log_failure(url):
+    with open('../smalltools/log.txt', 'a', encoding='utf-8') as log_file:
+        log_file.write(f"Failed to fetch: {url}\n")
+
+
+async def get_paper_urls(page_url: str, layout_url: str) -> list[str]:
+    html_string = await scrape_api(layout_url)
+    if html_string is None:
+        print('html_string is none')
         return []
+    try:
+        doc = pq(html_string)
+        # 找到 class 为 'news-list' 的 ul 下的所有 li 元素
+        items = list(doc('.news-list').children('li').items())
+        # print(items)
+        # 提取 li 元素中的 a 标签的 href 属性
+        hrefs = [li('a').attr('href') for li in items]
+        # print(hrefs)
+        # 组合成完整的 URL
+        urls = [urljoin(page_url, href) for href in hrefs]
+        # print(f'文章{urls}')
+        # print(urls)
+        return urls
+    except Exception as e:
+        print(f"Error parsing paper URLs from {layout_url}: {e}")
+        return []  # 确保在出错时返回一个空列表
 
-    bsobj = bs4.BeautifulSoup(html, 'html.parser')
-    temp = bsobj.find('div', attrs={'id': 'pageList'})
-    if temp:
-        pageList = temp.ul.find_all('div', attrs={'class': 'right_title-name'})
-    else:
-        pageList = bsobj.find('div', attrs={'class': 'swiper-container'}).find_all('div',
-                                                                                   attrs={'class': 'swiper-slide'})
 
-    linkList = []
-    for page in pageList:
-        link = page.a["href"]
-        url = f'http://paper.people.com.cn/rmrb/html/{year}-{month}/{day}/{link}'
-        linkList.append(url)
-
-    return linkList
-
-
-def getTitleList(year, month, day, pageUrl):
-    '''
-    功能：获取报纸某一版面的文章链接列表
-    参数：年，月，日，该版面的链接
-    '''
-    html = fetchUrl(pageUrl)
-    if not html:
+async def get_layout_urls(url: str, date_string: str) -> list[str]:
+    html_string = await scrape_api(url)
+    if html_string is None:
         return []
-
-    bsobj = bs4.BeautifulSoup(html, 'html.parser')
-    temp = bsobj.find('div', attrs={'id': 'titleList'})
-    if temp:
-        titleList = temp.ul.find_all('li')
-    else:
-        titleList = bsobj.find('ul', attrs={'class': 'news-list'}).find_all('li')
-
-    linkList = []
-    for title in titleList:
-        tempList = title.find_all('a')
-        for temp in tempList:
-            link = temp["href"]
-            if 'nw.D110000renmrb' in link:
-                url = f'http://paper.people.com.cn/rmrb/html/{year}-{month}/{day}/{link}'
-                linkList.append(url)
-
-    return linkList
+    try:
+        doc = pq(html_string)
+        # 直接查找 class 为 'swiper-slide' 的 div 下的 a 标签
+        items = list(doc('.swiper-container a').items())
+        # 提取每个 a 标签的 href 属性
+        hrefs = [a.attr('href') for a in items]
+        # print(hrefs)
+        # 组合成完整的 URL
+        urls = [urljoin(BASE_URL, f'{date_string}/{href}') for href in hrefs]
+        # print(urls)
+        return urls
+    except Exception as e:
+        print(f'{url}爬取错误: {e}')
+        return []  # 在出错时返回一个空列表
 
 
-def getContent(html):
-    '''
-    功能：解析 HTML 网页，获取新闻的文章内容
-    参数：html 网页内容
-    '''
-    bsobj = bs4.BeautifulSoup(html, 'html.parser')
+async def save_paper(date, title, text):
+    folder_path = os.path.join("人民日报", date)  # 将文章按日期保存在相应的文件夹里
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
 
-    # 获取文章 标题
-    title = f'{bsobj.h3.text}\n{bsobj.h1.text}\n{bsobj.h2.text}\n'
+    valid_title = "".join(c for c in title if c.isalnum() or c in (' ', '-', '_')).strip()
+    if valid_title == '':
+        print(f'{date}: 无标题')
+        return
 
-    # 获取文章 内容
-    pList = bsobj.find('div', attrs={'id': 'ozoom'}).find_all('p')
-    content = '\n'.join([p.text for p in pList])
+    file_path = os.path.join(folder_path, f"{valid_title}.txt")
 
-    # 返回结果 标题+内容
-    return title + content
+    async with aiofiles.open(file_path, 'w', encoding='utf-8') as f:
+        await f.write(f"Title: {title}\nDate: {date}\n\n{text}")
 
 
-def saveFile(content, path, filename):
-    '''
-    功能：将文章内容 content 保存到本地文件中
-    参数：要保存的内容，路径，文件名
-    '''
-    # 如果没有该文件夹，则自动生成
-    if not os.path.exists(path):
-        os.makedirs(path)
+async def get_papers(url, date):
+    html_string = await scrape_api(url)
+    if html_string is None:
+        print(f"{url} is None")
+        return
+    try:
+        html_string = re.sub(r'/\*.*?\*/', '', html_string, flags=re.DOTALL)
+        doc = pq(html_string)
 
-    # 保存文件
-    with open(os.path.join(path, filename), 'w', encoding='utf-8') as f:
-        f.write(content)
+        title = doc('.article h1').text()
+        text = doc('#ozoom').text()
 
-
-def download_rmrb(year, month, day, destdir=''):
-    '''
-    功能：爬取《人民日报》网站 某年 某月 某日 的新闻内容，并保存在 指定目录下
-    参数：年，月，日，文件保存的根目录
-    '''
-    pageList = getPageList(year, month, day)
-    for page in pageList:
-        titleList = getTitleList(year, month, day, page)
-        for url in titleList:
-            # 获取新闻文章内容
-            html = fetchUrl(url)
-            if not html:
-                continue
-            content = getContent(html)
-
-            # 生成保存的文件路径及文件名
-            temp = url.split('_')[2].split('.')[0].split('-')
-            pageNo = temp[1]
-            titleNo = temp[0] if int(temp[0]) >= 10 else '0' + temp[0]
-            path = os.path.join(destdir, f'{year}{month}{day}')
-            fileName = f'{year}{month}{day}_{pageNo}_{titleNo}.txt'
-
-            # 保存文件
-            saveFile(content, path, fileName)
+        await save_paper(date, title, text)
+    except Exception as e:
+        print(f"Error while getting papers from {url}: {e}")
 
 
-def gen_dates(b_date, days):
-    day = datetime.timedelta(days=1)
-    for i in range(days):
-        yield b_date + day * i
+async def main():
+    global session
+    async with aiohttp.ClientSession() as session:  # 在这里创建session
+        start_date = datetime(2023, 4, 11)
+        end_date = datetime(2023, 12, 31)
+        current_date = start_date
 
+        while current_date <= end_date:
+            date_string = current_date.strftime('%Y-%m/%d')
+            date_save_format = current_date.strftime('%Y%m%d')
+            print(f"------------------page:{current_date.strftime('%Y/%m/%d/')}------------------")
+            page_url = urljoin(BASE_URL, f"{date_string}/nbs.D110000renmrb_01.htm")
+            layout_urls = await get_layout_urls(page_url, date_string)
+            paper_urls = []
+            for layout_url in layout_urls:
+                paper_urls.extend(await get_paper_urls(page_url, layout_url))
 
-def get_date_list(beginDate, endDate):
-    """
-    获取日期列表
-    :param start: 开始日期
-    :param end: 结束日期
-    :return: 开始日期和结束日期之间的日期列表
-    """
-    start = datetime.datetime.strptime(beginDate, "%Y%m%d")
-    end = datetime.datetime.strptime(endDate, "%Y%m%d")
+            scrape_index_tasks = [asyncio.create_task(get_papers(paper_url, date_save_format)) for paper_url in
+                                  paper_urls]
+            await asyncio.gather(*scrape_index_tasks)
+            current_date += timedelta(days=3)
+            await asyncio.sleep(1)  # 模拟延迟
 
-    data = []
-    for d in gen_dates(start, (end - start).days + 1):
-        data.append(d)
-
-    return data
 
 if __name__ == '__main__':
-    '''
-    主函数：程序入口
-    '''
-    # 输入起止日期，爬取之间的新闻
-    beginDate = input('请输入开始日期 (格式: YYYYMMDD): ')
-    endDate = input('请输入结束日期 (格式: YYYYMMDD): ')
-    data = get_date_list(beginDate, endDate)
-
-    for d in data:
-        year = str(d.year)
-        month = str(d.month).zfill(2)
-        day = str(d.day).zfill(2)
-        download_rmrb(year, month, day)
-        print(f"爬取完成：{year}{month}{day}")
-        # time.sleep(2)
+    asyncio.run(main())
